6513aef9592e84fad254fc84afabc8b097af7145
davimc <davimatosc@hotmail.com>
2023-10-29 20:02:45 -0300

    header

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import CardViewer from './components/CardViewer'
+import Header from './components/Header'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='App'>
+      <Header/>
+      <div className='container'>
+        <CardViewer/>  
+      </div>      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import logo from '../../assets/ppay-icon.png'
+import { AiFillLinkedin as InIcon} from 'react-icons/ai'
+
+import './styles.css'
+
+function Header() {
+    const style = { backgroundColor: "white", fontSize: "1.2em",borderRadius: "30%", color: "blue", padding:"0.5px", margin: "1px 3px 1px 5px"}
+
+    return (
+        <header>
+            <div className="logo-container">
+                <img src={logo} alt="Picpay"/>
+                <h1>Picpay Simplificado</h1>
+            </div>
+            <p className='dev-contact'> Desenvolvido por
+                <div className='dev-contact-link'>
+                <InIcon style = {style}/>
+                    <a href="https://www.linkedin.com/in/davi-matos-carvalho/"> davi-In</a>
+                </div>
+            </p>
+        </header>
+    )
+}
+export default Header
